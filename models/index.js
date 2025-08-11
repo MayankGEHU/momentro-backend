ba35c0ad5f4a8b192eb06243202718a0b20f7cba
MayankGEHU <minku6195@gmail.com>
2025-08-11 18:15:34 +0530

    minor changes

--- models/index.js
+++ models/index.js
@@ -13,24 +13,25 @@ const sequelize = new Sequelize(
   }
 );
 
-sequelize.authenticate()
+sequelize
+  .authenticate()
   .then(() => console.log("✅ PostgreSQL connected"))
   .catch((err) => console.error("❌ Connection error:", err));
+
 
 const User = require("./User")(sequelize, DataTypes);
 const TimeSlot = require("./TimeSlot")(sequelize, DataTypes);
 const Habit = require("./Habit")(sequelize, DataTypes);
 const UserFavPic = require("./UserFavPic")(sequelize, DataTypes);
 
-// Setup associations
 User.hasMany(TimeSlot, { foreignKey: "userId", onDelete: "CASCADE" });
 TimeSlot.belongsTo(User, { foreignKey: "userId" });
 
 User.hasMany(Habit, { foreignKey: "userId", onDelete: "CASCADE" });
 Habit.belongsTo(User, { foreignKey: "userId" });
 
-// Add association for UserFavPic
 User.hasMany(UserFavPic, { foreignKey: "userId", onDelete: "CASCADE" });
 UserFavPic.belongsTo(User, { foreignKey: "userId" });
+
 
 module.exports = { sequelize, User, TimeSlot, Habit, UserFavPic };
